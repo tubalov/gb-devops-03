05c15b54ede19aa372c4b71edc4cc40da89fabca
tubalov <tubalov@gmail.com>
2021-05-23 13:18:02 +0300

    Update hw07.py

--- lesson4/hw07.py
+++ lesson4/hw07.py
@@ -4,10 +4,11 @@
 а в цикле необходимо выводить только первые n чисел, начиная с 1! и до n!.
 Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 '''
+from math import factorial
 
 def fact(n):
     for i in range(1,n+1):
-        yield i
+        yield factorial(i)
 
 for el in fact(4):
     print(el)
